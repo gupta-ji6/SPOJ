77a1cb45ad9b1322322c705cc68746c7cbf72af3
Ayush Gupta <ayushgupta197@gmail.com>
2018-09-07 11:56:40 +0530

    Added TEST.cpp

---
+++ TEST.cpp
@@ -0,0 +1,17 @@
+//Problem - TEST
+//Life, the Universe, and Everything
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i = 0;
+	while (1) {
+		cin >> i;
+		if (i==42)
+			break;
+		else
+			cout<<i<<endl;
+	}
+	return 0;
+}
